f9882dbc2939a40211c10a6e1208634ededcc42d
Shawn Presser <shawnpresser@gmail.com>
2022-11-18 00:33:41 -0600

    Update

--- pylibtpu.py
+++ pylibtpu.py
@@ -5,16 +5,32 @@ libtpu = ctypes.cdll.LoadLibrary("/usr/lib/libtpu.so")
 
 FuncPtr = ctypes.CFUNCTYPE(None)
 
+class TpuDriver(ctypes.Structure):
+  pass
+
+TpuDriver_p = ctypes.POINTER(TpuDriver)
+
+class TpuSystemInfo(ctypes.Structure):
+  _fields_ = [
+      ("bytes", ctypes.POINTER(ctypes.c_char)),
+      ("size", ctypes.c_int32),
+      ]
+  def __bytes__(self):
+    if self.bytes:
+      return self.bytes[0:self.size]
+
+TpuSystemInfo_p = ctypes.POINTER(TpuSystemInfo)
+
 
 @dataclasses.dataclass
 class TpuDriverFn(ctypes.Structure):
   _fields_ = [
-    ("TpuDriver_Open", FuncPtr),
-    ("TpuDriver_Close", FuncPtr),
+    ("TpuDriver_Open", ctypes.CFUNCTYPE(TpuDriver_p, ctypes.c_char_p)),
+    ("TpuDriver_Close", ctypes.CFUNCTYPE(None, TpuDriver_p)),
     ("TpuDriver_Reset", FuncPtr),
     ("TpuDriver_ComputeLinearizedBytesFromShape", FuncPtr),
-    ("TpuDriver_QuerySystemInfo", FuncPtr),
-    ("TpuDriver_FreeSystemInfo", FuncPtr),
+    ("TpuDriver_QuerySystemInfo", ctypes.CFUNCTYPE(TpuSystemInfo_p, TpuDriver_p)),
+    ("TpuDriver_FreeSystemInfo", ctypes.CFUNCTYPE(None, TpuSystemInfo_p)),
     ("TpuDriver_LinearizeShape", FuncPtr),
     ("TpuDriver_DelinearizeShape", FuncPtr),
     ("TpuDriver_CompileProgram", FuncPtr),
@@ -54,10 +70,14 @@ libtpu.TpuDriver_Initialize.restype = None
 driver_fn = TpuDriverFn()
 
 
-libtpu.TpuDriver_Initialize(driver_fn, False)
+libtpu.TpuDriver_Initialize(driver_fn, True)
 
 driver_fn.TpuDriver_Version.errcheck = lambda version: version.decode('utf8')
 
 print(driver_fn.TpuDriver_Version())
 print(driver_fn)
+
+
+print("opening local://")
+driver = driver_fn.TpuDriver_Open("local://")
 
